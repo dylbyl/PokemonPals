9189844f408c622089ae2275c7952229cadc31b4
Dylan Bishop <mdylbishop@gmail.com>
2020-09-29 10:12:56 -0400

    users now see their collection on dex details

--- PokemonPals/Models/ViewModels/CaughtPokemonCreateViewModel.cs
+++ PokemonPals/Models/ViewModels/CaughtPokemonCreateViewModel.cs
@@ -8,8 +8,8 @@ namespace PokemonPals.Models.ViewModels
 {
     public class CaughtPokemonCreateViewModel
     {
-        public Pokemon SelectedPokemon { get; set; }
-        public CaughtPokemon PokemonToAdd { get; set; }
-        public List<SelectListItem> Genders { get; set; }
+        public Pokemon SelectedPokemon { get; set; } = new Pokemon();
+        public CaughtPokemon PokemonToAdd { get; set; } = new CaughtPokemon();
+        public List<SelectListItem> Genders { get; set; } = new List<SelectListItem>();
     }
 }
---
+++ PokemonPals/Models/ViewModels/CaughtPokemonDexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokemonPals.Models.ViewModels
+{
+    public class CaughtPokemonDexViewModel
+    {
+        public Pokemon SelectedPokemon { get; set; } = new Pokemon();
+
+        public List<CaughtPokemon> UserCollection { get; set; } = new List<CaughtPokemon>();
+    }
+}
