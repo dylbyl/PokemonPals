f30beab7fb365d52f9c2e3d10ab11e52cc6db96e
Dylan Bishop <mdylbishop@gmail.com>
2020-10-05 14:57:40 -0400

    can view favorites on profile

---
+++ PokemonPals/Models/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokemonPals.Models.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public ApplicationUser ViewedUser = new ApplicationUser();
+        public Boolean isOwnProfile = false;
+        public List<CaughtPokemon> UserCollection = new List<CaughtPokemon>();
+        public List<int> CaughtPokemonIDs = new List<int>();
+        public List<CaughtPokemon> FavoritePokemon = new List<CaughtPokemon>();
+        public List<CaughtPokemon> TradePokemon = new List<CaughtPokemon>();
+    }
+}
